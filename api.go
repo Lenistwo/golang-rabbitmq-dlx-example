e8f619047bf7a6f420358d555b4bde0e8377572b
Kacper <kacperek1003@gmail.com>
2021-04-21 19:49:28 +0200

    Added Endpoint For Checking Queue Statistics

--- api.go
+++ api.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"github.com/lenistwo/config"
 	"github.com/lenistwo/model"
 	"github.com/lenistwo/util"
 	"github.com/streadway/amqp"
 	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
 
 var (
@@ -17,7 +22,32 @@ func CreateRouter(amqConnection *amqp.Connection) *gin.Engine {
 	connection = amqConnection
 	server := gin.Default()
 	server.POST("/send", sendMail)
+	server.GET("/queue", queueStats)
 	return server
+}
+
+func queueStats(c *gin.Context) {
+	request, err := http.NewRequest("GET", buildQueueURL(), nil)
+	util.CheckError(err)
+	request.Header.Set("Authorization", prepareAuthorizationHeader())
+	client := &http.Client{}
+	apiResponse, err := client.Do(request)
+
+	if err != nil {
+		c.JSON(500, gin.H{"Message": "Internal Server Error"})
+		return
+	}
+
+	body, err := ioutil.ReadAll(apiResponse.Body)
+	var response map[string]interface{}
+	err = json.Unmarshal(body, &response)
+
+	if err != nil {
+		c.JSON(500, gin.H{"Message": "Internal Server Error"})
+		return
+	}
+
+	c.JSON(200, response)
 }
 
 func sendMail(context *gin.Context) {
@@ -37,4 +67,12 @@ func sendMail(context *gin.Context) {
 
 	mail.Publish(connection)
 	context.JSON(200, gin.H{"Message": "Scheduled Mail"})
+}
+
+func buildQueueURL() string {
+	return "http://" + os.Getenv("RABBIT_HOST") + ":" + os.Getenv("RABBIT_MANAGEMENT_PORT") + "/api/queues/%2f/" + config.DelayedMailQueueName
+}
+
+func prepareAuthorizationHeader() string {
+	return "Basic " + base64.StdEncoding.EncodeToString([]byte(os.Getenv("RABBIT_USERNAME")+":"+os.Getenv("RABBIT_PASSWORD")))
 }
